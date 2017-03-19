8743ac3cf68fc98f974f2fd112dce48088a4fb5e
David Xie <david30xie@gmail.com>
2017-03-19 01:04:51 +0000

    make http session and security work

---
+++ src/main/java/com/daimaduan/config/HttpSessionConfig.java
@@ -0,0 +1,13 @@
+package com.daimaduan.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.session.data.mongo.JdkMongoSessionConverter;
+import org.springframework.session.data.mongo.config.annotation.web.http.EnableMongoHttpSession;
+
+@EnableMongoHttpSession
+public class HttpSessionConfig {
+    @Bean
+    public JdkMongoSessionConverter jdkMongoSessionConverter() {
+        return new JdkMongoSessionConverter();
+    }
+}
